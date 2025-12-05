e5ac973763ff18efd97e716dea965df93047e48e
yukkuri <reityan.sakura0104@gmail.com>
2025-12-05 18:13:05 +0900

    初期化

--- C5/script.js
+++ C5/script.js
@@ -1,14 +1,3 @@
-const btn = document.getElementById("btn")
-const text = document.getElementById("text")
-btn.addEventListener("click",()=>{
-    if(text.textContent=="ライトモードテスト"){
-        document.body.style.background="#000"
-        document.body.style.color="#FFF"
-        text.textContent="ナイトモードテスト"
-    }
-    else if(text.textContent=="ナイトモードテスト"){
-        document.body.style.background="#FFF"
-        document.body.style.color="#000"
-        text.textContent="ライトモードテスト"
-    }
-})
+function swich(){
+    
+}
