5b22a408cb7df15b1fc26dbb373982b6f40bfcc2
yukkuri <reityan.sakura0104@gmail.com>
2025-12-07 08:56:33 +0900

    ダークモード

--- C5/script.js
+++ C5/script.js
@@ -1,3 +1,16 @@
-function swich(){
-    
+const text = document.getElementById("text");
+let light = true;
+function swich() {
+    if (light) {
+        light = false;
+        text.textContent = "ダークモードテスト";
+        document.body.style.backgroundColor = "#000";
+        text.style.color = "#FFF"
+    }
+    else {
+        light = true;
+        text.textContent = "ライトモードテスト";
+        document.body.style.backgroundColor = "#FFF";
+        text.style.color = "#000"
+    }
 }
